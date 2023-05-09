df4be33316110d30cdc0211581f7d993381f8f06
Vansh Awasthi <75345433+VanshAwasthi@users.noreply.github.com>
2023-05-09 12:47:02 +0530

    Creating Routes & Schema for Storing Notes & Users

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Vansh!')
-})
+
+//Available Routes
+app.use('/api/auth',require ('./routes/auth'))//app.use to make connectionn with routes
+app.use('/api/notes',require ('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
