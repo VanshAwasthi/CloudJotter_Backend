a19b24df9afcf1a03b5f91c577da682fcbfad979
Vansh Awasthi <75345433+VanshAwasthi@users.noreply.github.com>
2023-05-11 23:12:01 +0530

    Creating Login Endpoint & sending auth token

--- routes/auth.js
+++ routes/auth.js
@@ -55,7 +55,7 @@ router.post(
 
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some error occured");
+      res.status(500).send("Internal Server Error");
     }
 
     // .then(user => res.json(user))
@@ -63,5 +63,55 @@ router.post(
     // res.json({error : 'Please enter a unique value for email', message: err.message})})
   }
 );
+
+// Authenticate a User using POST "/api/auth/login" . No login required
+router.post(
+  "/login",
+  [
+  
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password cannot be blanked").exists()
+   
+  ],
+  async (req, res) => {
+
+    //If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email,password} = req.body;
+
+    try {
+      let user = await User.findOne({email});
+      if(!user){
+        return res.status(400).json({ errors:"Please try to login with correct credentials"});
+      }
+
+      const passwordCompare = await bcrypt.compare(password,user.password);
+      if(!passwordCompare){
+        return res.status(400).json({ errors:"Please try to login with correct credentials"});
+      }
+
+      const payload ={
+        user:{
+          id: user.id
+        }
+      }
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
+      const authtoken = jwt.sign(data,JWT_SECRET );
+      res.json({authtoken});
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+
+  })
 
 module.exports = router;
