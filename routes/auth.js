02d0523d94915048702a783fe303b856c92ed131
Vansh Awasthi <75345433+VanshAwasthi@users.noreply.github.com>
2023-05-12 00:03:24 +0530

    Creating a middleware to decode user from a JWT

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+var fetchuser = require('../middleware/fetchuser');
 
-const JWT_SECRET = 'vanshIsaGood@002'
+const JWT_SECRET = "vanshIsaGood@002";
 
 //router.(dot) karke isleye likh rhe hai kyo ki ye ek particular route ke leye hai
-// Create a User using POST "/api/auth/createuser" . No login required
+// ROUTE 1: Create a User using POST "/api/auth/createuser" . No login required
 router.post(
   "/createuser",
   [
@@ -32,8 +33,8 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email alreay exists" });
       }
-      const salt = await bcrypt.genSaltSync(10); 
-      const secPass = await bcrypt.hash(req.body.password,salt);
+      const salt = await bcrypt.genSaltSync(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
 
       //Create a new user
       user = await User.create({
@@ -43,16 +44,13 @@ router.post(
       });
 
       const data = {
-        user:{
-          id: user.id
-        }
-      }
-      const authtoken = jwt.sign(data,JWT_SECRET );
-      
-
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
       // res.json(user);
-      res.json({authtoken});
-
+      res.json({ authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal Server Error");
@@ -64,54 +62,69 @@ router.post(
   }
 );
 
-// Authenticate a User using POST "/api/auth/login" . No login required
+// ROUTE 2: Authenticate a User using POST "/api/auth/login" . No login required
 router.post(
   "/login",
   [
-  
     body("email", "Enter a valid Email").isEmail(),
-    body("password", "Password cannot be blanked").exists()
-   
+    body("password", "Password cannot be blanked").exists(),
   ],
   async (req, res) => {
-
     //If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const {email,password} = req.body;
+    const { email, password } = req.body;
 
     try {
-      let user = await User.findOne({email});
-      if(!user){
-        return res.status(400).json({ errors:"Please try to login with correct credentials"});
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: "Please try to login with correct credentials" });
       }
 
-      const passwordCompare = await bcrypt.compare(password,user.password);
-      if(!passwordCompare){
-        return res.status(400).json({ errors:"Please try to login with correct credentials"});
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ errors: "Please try to login with correct credentials" });
       }
 
-      const payload ={
-        user:{
-          id: user.id
-        }
-      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
       const data = {
-        user:{
-          id: user.id
-        }
-      }
-      const authtoken = jwt.sign(data,JWT_SECRET );
-      res.json({authtoken});
-
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal Server Error");
     }
+  }
+);
 
-  })
+// ROUTE 3: Get logged in User Details using POST "/api/auth/getuser" .login required(mtlb jwt token bhejna padhega)
+router.post(
+  "/getuser",fetchuser,
+  async (req, res) => {
+    try {
+      userId = req.user.id;
+      const user = await User.findById(userId).select("-password");
+      res.send(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 
 module.exports = router;
