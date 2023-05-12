b48aaa28f360d4ffa83d40699631d5c2804d8e2e
Vansh Awasthi <75345433+VanshAwasthi@users.noreply.github.com>
2023-05-12 17:32:57 +0530

    Updating an existing Note

--- routes/notes.js
+++ routes/notes.js
@@ -51,4 +51,28 @@ router.post(
   }
 );
 
+// ROUTE 3: Update an Existing Note using PUT "/api/notes/updatenote/:id" . login required
+router.put(
+  "/updatenote/:id",
+  fetchuser,
+  async (req, res) => {
+      const {title,description,tag} = req.body;
+      //Create a newNote object
+      const newNote = {};
+      if(title){newNote.title = title};
+      if(description){newNote.description = description};
+      if(tag){newNote.tag = tag};
+
+      //Find the note to be Updated and Update it
+      let note  = await Note.findById(req.params.id);
+      if(!note){return res.status(404).send("Not Found")}
+      if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+      }
+
+      note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote},{new: true} )//new: true isleye agar koi new object ata hai toh vo create hojaega
+      res.json({note});
+  })  
+
+
 module.exports = router;
