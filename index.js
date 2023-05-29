562471dda635963501c88eb217ba6d36467595b3
Vansh Awasthi <75345433+VanshAwasthi@users.noreply.github.com>
2023-05-29 16:23:32 +0530

    cors

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // index.js ek express server rahega
 const connectToMongo = require("./db");
-const express = require('express')
+const express = require("express");
+var cors = require('cors')
 
 connectToMongo();
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
+
+app.use(cors())
 
 //If want to use request.body then use this middleware
-app.use(express.json())
+app.use(express.json());
 
 //Available Routes
-app.use('/api/auth',require ('./routes/auth'))//app.use to make connectionn with routes
-app.use('/api/notes',require ('./routes/notes'))
+app.use("/api/auth", require("./routes/auth")); //app.use to make connectionn with routes
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`CloudJotter Backend listening on port http://localhost:${port}`);
+});
